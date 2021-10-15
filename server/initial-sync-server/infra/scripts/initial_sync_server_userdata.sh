aa8d1e6f968f2c0553baa780f5561f21e1d8ff23
dylan <dylanhogg@gmail.com>
2021-10-15 18:45:57 +1100

    Fix userdata.

--- server/initial-sync-server/infra/scripts/initial_sync_server_userdata.sh
+++ server/initial-sync-server/infra/scripts/initial_sync_server_userdata.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # amd64 / i3.xlarge
 # wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.9-eae3b194.tar.gz
 # tar -xzf geth-linux-amd64-1.10.9-eae3b194.tar.gz
@@ -6,10 +8,11 @@
 # arm64 / t4g.medium
 wget https://gethstore.blob.core.windows.net/builds/geth-linux-arm64-1.10.9-eae3b194.tar.gz
 tar -xzf geth-linux-arm64-1.10.9-eae3b194.tar.gz
-mv geth-linux-arm64-1.10.9-eae3b194/geth ~/
+# mv geth-linux-arm64-1.10.9-eae3b194/geth /home/ec2-user/geth
+mv geth-linux-arm64-1.10.9-eae3b194/geth /root/geth
 
 # t4g.medium (TODO)
-~/geth --syncmode snap --nousb
+/root/geth --syncmode snap --nousb
 
 # i3.xlarge (REVIEW)
 # ./geth --datadir /mnt/nvm/ether --syncmode snap --maxpeers 100 --cache 28000 --gcmode archive
